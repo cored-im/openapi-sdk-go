966a691566cfeb8dce1b9b53cc6acb4a8ba89714
xalanq <xalanq@gmail.com>
2026-03-30 08:49:30 +0800

    upd(common): update SPDX-License-Identifier

--- core/version.go
+++ core/version.go
@@ -1,7 +1,10 @@
+// Copyright (c) 2026 Cored Limited
+// SPDX-License-Identifier: Apache-2.0
+
 package cocore
 
 // SDK version
-const Version = "0.28.100"
+const Version = "0.28.101"
 
 // User-Agent
 const UserAgent = "cored-openapi-sdk-go/" + Version
